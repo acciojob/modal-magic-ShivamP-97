ac10dd0d83890470ff8b0a640d4b7984b06e0119
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-01 22:41:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const openModalBtn = document.querySelector('.open-modal-btn');
+const modalOverlay = document.getElementById('modalOverlay');
+const closeBtn = document.getElementById('closeBtn');
+
+openModalBtn.addEventListener('click', () => {
+  modalOverlay.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', () => {
+  modalOverlay.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target === modalOverlay) {
+    modalOverlay.style.display = 'none';
+  }
+});
